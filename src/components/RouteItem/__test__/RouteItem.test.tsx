7e9cf695c203589e7b963b600deb6c597e91d17d
Maverick <supacode@gmail.com>
2022-04-15 04:40:25 +0100

    Add failing tests for RouteItem component

---
+++ src/components/RouteItem/__test__/RouteItem.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { RouteItem } from '../';
+import { ScheduleStrategy } from '../../../types/Schedule';
+import stops from '../../../mocks/stops.json';
+
+const mockProps = {
+  schedulingStrategy: ScheduleStrategy.Fixed,
+  isLastStop: false,
+  index: 0,
+  stop: stops[0],
+  canAddCargo: true,
+};
+
+beforeEach(() => {
+  // eslint-disable-next-line testing-library/no-render-in-setup
+  render(<RouteItem {...mockProps} />);
+});
+
+describe('RouteItem', () => {
+  it('should have edit button by default', () => {
+    screen.getByRole('button', { name: /edit/i });
+  });
+
+  it('should show schedule form when edit button is clicked', () => {
+    const editButton = screen.getByRole('button', { name: /edit/i });
+
+    userEvent.click(editButton);
+
+    expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument();
+
+    expect(
+      screen.queryByRole('button', { name: /edit/i }),
+    ).not.toBeInTheDocument();
+  });
+});
