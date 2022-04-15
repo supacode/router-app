91c02d5b4886ef455ffd7fad01dbd8aca1ad8de5
Maverick <supacode@gmail.com>
2022-04-15 10:22:34 +0100

    Add AddCargoForm component
    Style housing App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,52 @@
+import { useState } from 'react';
+import { RouteDetails } from '../components/RouteDetails/RouteDetails';
+import { ScheduleStrategy } from '../types/Schedule';
+import stops from '../data/stops.json';
 import './App.scss';
 
 export const App: React.FC = () => {
-  return <></>;
+  const [schedulingStrategy, setSchedulingStrategy] =
+    useState<ScheduleStrategy>(ScheduleStrategy.Flexible);
+  const [canAddCargo, setCanAddCargo] = useState<boolean>(false);
+
+  const schedulingStrategyHandler = (
+    evt: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    const selectedStrategy = evt.target.value as ScheduleStrategy;
+
+    setSchedulingStrategy(selectedStrategy);
+  };
+
+  const canAddCargoHandler = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setCanAddCargo(!canAddCargo);
+  };
+
+  return (
+    <div className="app-container">
+      <div className="app-container__control">
+        <select
+          defaultValue={schedulingStrategy}
+          onChange={schedulingStrategyHandler}
+        >
+          <option value={ScheduleStrategy.Fixed}>Fixed</option>
+          <option value={ScheduleStrategy.SemiFlexible}>Semi Flexible</option>
+          <option value={ScheduleStrategy.Flexible}>Flexible</option>
+        </select>
+        <label>
+          <input
+            type="checkbox"
+            checked={canAddCargo}
+            onChange={canAddCargoHandler}
+          />
+          Can add cargo
+        </label>
+      </div>
+
+      <RouteDetails
+        stops={stops}
+        schedulingStrategy={schedulingStrategy}
+        canAddCargo={canAddCargo}
+      />
+    </div>
+  );
 };
--- src/components/AddCargoForm/AddCargoForm.tsx
+++ src/components/AddCargoForm/AddCargoForm.tsx
@@ -1,3 +1,7 @@
+import { useCallback } from 'react';
+import { Button } from '../Base/Button';
+import { Input } from '../Base/Input';
+import { Select } from '../Base/Select/Select';
 import './AddCargoForm.scss';
 
 type AddCargoFormProps = {
@@ -7,5 +11,63 @@ type AddCargoFormProps = {
 export const AddCargoForm: React.FC<AddCargoFormProps> = ({
   cancelHandler,
 }) => {
-  return <p>Add Cargo Form</p>;
+  const cancelHandlerCallback = useCallback(() => {
+    cancelHandler && cancelHandler();
+  }, [cancelHandler]);
+
+  const onSubmitHandler = useCallback(
+    (evt: React.FormEvent<HTMLFormElement>) => {
+      evt.preventDefault();
+      // Out of scope
+    },
+    [],
+  );
+
+  return (
+    <form className="form" onSubmit={(e) => onSubmitHandler}>
+      <legend className="form__title">Cargo Item</legend>
+
+      <div className="form__form-group form__form-group--first-row">
+        <Select id="type" label="Type">
+          <option value="">Please select an option</option>
+        </Select>
+
+        <Select id="uploading-state" label="Uploading Stop">
+          <option value="">Please select an option</option>
+        </Select>
+
+        <Input label="PO number" type="text" id="po-number" />
+      </div>
+
+      <div className="form__form-group form__form-group--second-row">
+        <Input type="text" label="Quantity" id="quantity" />
+
+        <Input type="text" label="Total weight" append="kg" id="total-weight" />
+
+        <div className="form__separator" />
+
+        <Input type="text" label="Length" append="cm" id="length" />
+
+        <Input type="text" label="Width" append="cm" id="width" />
+
+        <Input type="text" label="Height" append="cm" id="height" />
+      </div>
+
+      <div className="form__form-control">
+        <Button
+          variant="tertiary"
+          className="form__btn"
+          text="Cancel"
+          onClick={cancelHandlerCallback}
+        />
+
+        <Button
+          variant="primary"
+          text="Save"
+          className="form__btn form__cta"
+          onClick={cancelHandlerCallback}
+        />
+      </div>
+    </form>
+  );
 };
